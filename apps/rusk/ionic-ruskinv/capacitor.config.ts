afb0cac95bbce3156db6bf71cf187ce09e2f2f98
Wayne Stidolph <wayne@stidolph.com>
2022-06-05 16:43:12 -0700

    see appUd for capacitor, just a lark now

--- apps/rusk/ionic-ruskinv/capacitor.config.ts
+++ apps/rusk/ionic-ruskinv/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.sse.inventory',
   appName: 'ruskinv',
   webDir: 'www',
   bundledWebRuntime: false,
