91f04278b0e8887c89bf3d9e488a7f1ff08a11be
Wayne Stidolph <wayne@stidolph.com>
2022-06-27 08:53:52 -0700

    add annotorious better polygon

--- libs/xplat/features/src/lib/ruskdata/services/anno.service.ts
+++ libs/xplat/features/src/lib/ruskdata/services/anno.service.ts
@@ -5,10 +5,18 @@ import { BehaviorSubject } from 'rxjs';
 import { Annotorious } from '@recogito/annotorious';
 import Toolbar from '@recogito/annotorious-toolbar'
 import SelectorPack from '@recogito/annotorious-selector-pack'
+import BetterPolygon from '@recogito/annotorious-better-polygon'
 
 export enum DRAWTOOLS {
   FREEHAND='freehand', ELLIPSE='ellipse', CIRCLE='circle',
   POINT='point', RECT='rect', POLYGON='polygon'
+}
+
+// annotation entry body structure
+export interface AEB {
+  type: string;
+  value: string;
+  purpose: string
 }
 
 /* provides and supports use of the Annotorius image annotator library
@@ -56,58 +64,104 @@ export class AnnoService {
 
     this._anno = new Annotorious(config);
 
+    // TODO fix PicturePage to pass in current server time
+    if(config['serverTime']) {
+      this._anno.setServerTime(config['serverTime'])
+    }
+
     //connect events
     this._anno.on('selectAnnotation', (a:any, shape:any) => {
-      console.log('selected');
+
+      const payload = {
+        id: a.id,
+        annotation: a,
+        shape: shape}
+      console.log('AS got selectAnnotation; a', a, ' shape', shape, ' payload:', payload);
       this._annoEvents$.next({type: 'selectAnnotation',
-                              payload:{ a: a,
-                              shape: shape}})
+                              payload})
     });
 
     this._anno.on('cancelSelected', (a:any) => {
-      console.log('cancel');
-      this._annoEvents$.next({type: 'cancelSelected', payload:{ a:a}})
+
+      const payload = {
+        id: a.id,
+        annotation: a
+      }
+      console.log('AS got cancelSelected', a, ' payload', payload);
+      this._annoEvents$.next({
+        type: 'cancelSelected',
+        payload})
     });
 
+    // essentially cancel prev selection, now select selected
     this._anno.on('changeSelected', (selected:any, previous:any) => {
-      console.log('changed from', previous, 'to', selected);
+      const payload = {
+        id: selected.id,
+        selected:selected, previous:previous
+      }
+      console.log('AS got changeSelected', payload);
       this._annoEvents$.next({type: 'changeSelected',
-        payload:{ selected, previous}})
+        payload})
     });
 
     this._anno.on('createAnnotation', (annotation:any) => {
-      console.log('created', annotation);
+      const payload = {
+        id: annotation.id,
+        annotation:annotation
+      }
+      console.log('AS got createAnnotation, payload:', payload);
         this._annoEvents$.next({type: 'createAnnotation', payload:{ annotation}})
     });
 
     this._anno.on('updateAnnotation', (annotation:any, previous:any) => {
-      console.log('updated', previous, 'with', annotation);
+      const payload = {
+        id: annotation.id,
+        annotation:annotation,
+        previous:previous
+      }
+      console.log('AS got updateAnnotation', payload);
       this._annoEvents$.next({type: 'updateAnnotation',
-        payload:{ annotation, previous}})
+        payload})
     });
 
     this._anno.on('clickAnnotation', (annotation:any, shape:any) => {
-      console.log('clicked', annotation);
+      const payload = {
+        id: annotation.id,
+        annotation:annotation, shape:shape
+      }
+      console.log('AS got clickAnnotation', payload);
       this._annoEvents$.next({type: 'clickAnnotation',
-        payload:{ annotation, shape}})
+        payload})
     });
 
     this._anno.on('deleteAnnotation', (annotation:any) => {
-      console.log('deleted', annotation);
+      const payload = {
+        id: annotation.id,
+        annotation: annotation
+      }
+      console.log('AS got deleteAnnotation', payload);
       this._annoEvents$.next({type: 'deleteAnnotation',
-        payload:{ annotation}})
+        payload})
     });
 
     this._anno.on('mouseEnterAnnotation', (annotation:any) => {
-      console.log('enter', annotation);
+      const payload = {
+        id: annotation.id, // I hope
+        annotation: annotation
+      }
+      console.log('AS got mouseEnterAnnotation', payload);
       this._annoEvents$.next({type: 'mouseEnterAnnotation',
-        payload:{ annotation}})
+        payload})
     });
 
     this._anno.on('mouseLeaveAnnotation', (annotation:any) => {
-      console.log('leave', annotation);
+      const payload = {
+        id: annotation.id,
+        annotation: annotation
+      }
+      console.log('AS got mouseLeaveAnnotation', payload);
       this._annoEvents$.next({type: 'mouseLeaveAnnotation',
-        payload:{ annotation}})
+        payload})
     });
 
     this._isSetup = true;
@@ -115,6 +169,7 @@ export class AnnoService {
     setupToolbar(containerId: string, drawtoolsList: string[]=[]) {
 
       SelectorPack(this._anno, drawtoolsList.length >0? {tools: drawtoolsList}:  undefined); // add in tools
+      BetterPolygon(this._anno);
       console.log('draw tools now', this._anno.listDrawingTools())
       Toolbar(this._anno, document.getElementById('AnnotoriousToolbarcontainer'));
     }
@@ -141,7 +196,34 @@ export class AnnoService {
       })
     }
 
+    deleteAnnotation(annoId: string) {
+      this._anno.removeAnnotation(annoId);
+    }
+
+    // highlight shape and open edit
+    selectAnnotation(annoId: string) {
+      this._anno.selectAnnotation(annoId);
+    }
+
     setDrawingTool(toolname: string) {
       this._anno.setDrawingTool(toolname);
     }
+
+    // utility data access functions
+    extractCommentsAndTags(ae: any): {comments: string[], tags: string[]} {
+      const anno = ae.payload.annotation
+      // ae can have 1 or multiple body,
+      // depends on what user fills out
+
+      const theBody: AEB[] = anno.body;
+      const commentArray = theBody.filter((bdyEl) => {return bdyEl.purpose === 'commenting' || bdyEl.purpose === 'replying'})
+        .map(val => {return val.value} );
+      const tagArray = theBody.filter((bdyEl) => {return bdyEl.purpose === 'tagging'})
+        .map(val => {return val.value});
+
+        return {comments: commentArray,
+                tags: tagArray}
+    }
+
+
 }
--- libs/xplat/features/src/lib/ruskdata/services/annotorius.d.ts
+++ libs/xplat/features/src/lib/ruskdata/services/annotorius.d.ts
@@ -1,3 +1,4 @@
 declare module '@recogito/annotorious';
 declare module '@recogito/annotorious-toolbar';
-declare module '@recogito/annotorious-selector-pack'
+declare module '@recogito/annotorious-selector-pack';
+declare module '@recogito/annotorious-better-polygon';
