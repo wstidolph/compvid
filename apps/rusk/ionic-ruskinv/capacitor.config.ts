4fbc6098b0dfc86b0eed8c80108ff6f70621cb9c
Wayne Stidolph <wayne@stidolph.com>
2022-07-06 08:16:56 -0700

    chg capacitor config for local server support

--- apps/rusk/ionic-ruskinv/capacitor.config.ts
+++ apps/rusk/ionic-ruskinv/capacitor.config.ts
@@ -2,9 +2,13 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sse.ruskdata.ionicinv',
-  appName: 'PhotItems',
+  appName: 'PhotoItems',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url:'http://0.0.0.0:4200',
+    cleartext: true
+  }
 };
 
 export default config;
